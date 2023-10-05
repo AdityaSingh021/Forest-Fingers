49f77971f7eb0a562a647c33fb7dc85c7d596845
AdityaSingh021 <aditya877633@gmail.com>
2023-10-06 02:08:43 +0530

    React router problem

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import linkedin from './Components/images/linkedin.png';
 import insta from './Components/images/instagram.png';
 import git from './Components/images/github.png';
 import Info from './Components/Info';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import bg from './Components/images/bg.jpg';
 
@@ -21,7 +21,7 @@ function App() {
       <img className="tree-1" src={tree_1} alt="" />
       <img className="tree-1" src={tree_2} alt="" />
       <img className="birds" src={birds} alt="" />
-      <HashRouter>
+      <Router basename='/Forest-Fingers'>
         <Routes>
         <Route path="/Forest-Fingers" element={<TypeContainer />} />
         <Route path="/" element={<TypeContainer />} />
@@ -29,7 +29,7 @@ function App() {
           
           {/* Add more routes for additional pages */}
         </Routes>
-      </HashRouter>
+      </Router>
       {/* <div className="type-container"><TypeContainer /></div> */}
       <div className="footer">
         <div> <a className="linkedin" href="https://www.linkedin.com/in/aditya-singh22/">LinkedIn<img className='img' src={linkedin} alt="" height={30} width={30}/></a> </div>
